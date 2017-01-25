2ad32d6b90d101cdda35c9b79874ab95592be12f
Vamshidhar Dudi <vdudi3@gatech.edu>
2017-01-25 16:49:40 -0500

    map and flatmap functions on MAPs

--- Scala/src/com/Maps.scala
+++ Scala/src/com/Maps.scala
@@ -9,11 +9,19 @@ object Maps {
 
     for((k,v)<- states) printf("Key: %s, Value: %s\n", k, v)
 
-    for(x<-states) println("Key: "+ x._1 +", Value: "+ x._2)
+    for(x<-states1) println("Key: "+ x._1 +", Value: "+ x._2)
     var similar =""
     states.keys.foreach( (key)=>if(states1.contains(key)) similar += (key, (states1.get(key))))
     println("similar: "+ similar)
 
+
+    def h(k: String, v: String) = {
+      if(states1.contains(k)) Some(k->v) else None
+    }
+    //println(states.flatMap((kv) => if(kv._1 == states1.contains(kv._1))(kv._1,kv._2)))
+    println(states.flatMap(e => List(e)))
+    println(states.flatMap(e => h(e._1, e._2)))
+    println(states.map(e => h(e._1, e._2)))
 
 
 
